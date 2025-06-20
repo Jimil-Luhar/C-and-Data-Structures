2e3e42f2a08de296173884db81567622eca24657
Jimil Luhar <122004645+Jimil-Luhar@users.noreply.github.com>
2025-06-20 11:40:30 +0530

    Create addmatrix.c

---
+++ Assignment-5/addmatrix.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#define row 3
+#define col 3
+void main() {
+    int matrix1[row][col], matrix2[row][col], result[row][col];
+    printf("Input Matrix1:\n");
+    for (int i = 0; i < row; i++) {
+        for (int j = 0; j < col; j++) {
+            scanf("%d", &matrix1[i][j]);
+        }
+    }
+    printf("Input Matrix2:\n");
+    for (int i = 0; i < row; i++) {
+        for (int j = 0; j < col; j++) {
+            scanf("%d", &matrix2[i][j]);
+        }
+    }
+    printf("Matrix Sum:\n");
+    for (int i = 0; i < row; i++) {
+        for (int j = 0; j < col; j++) {
+            result[i][j] = matrix1[i][j] + matrix2[i][j];
+            printf("%d ", result[i][j]);
+        }
+        printf("\n");
+    }
+}
